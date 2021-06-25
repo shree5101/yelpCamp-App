807d65da62b32dd24a8ed7c4847606b23c35715d
shree5101 <shredya5101@gmail.com>
2021-06-25 07:29:10 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
+const Campground = require("./models/campgrounds");
+const { urlencoded } = require("express");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
+mongoose.connect("mongodb://localhost:27017/yelpApp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const app = express();
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get(
+  "/campgrounds",
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post(
+  "/campgrounds",
+  catchAsync(async (req, res, next) => {
+    const newCampground = new Campground(req.body);
+    await newCampground.save();
+    res.redirect(`/campgrounds/${newCampground._id}`);
+  })
+);
+
+app.get(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/show", { campground });
+  })
+);
+
+app.put(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, req.body, {
+      runValidators: true,
+      new: true,
+      useFindAndModify: false,
+    });
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+app.delete(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
+
+app.get(
+  "/campgrounds/:id/edit",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/edit", { campground });
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found!", 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something Went Wrong" } = err;
+  res.status(statusCode).send(message);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
---
+++ seeds/index.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Campground = require("../models/campgrounds");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelper");
+
+mongoose.connect("mongodb://localhost:27017/yelpApp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+
+  const price = Math.floor(Math.random() * 1000);
+
+  for (let i = 0; i < 50; i++) {
+    const camp = new Campground({
+      location: `${cities[i].city}, ${cities[i].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      price,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Feugiat nibh sed pulvinar proin. Et netus et malesuada fames ac. Dignissim suspendisse in est ante. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla.",
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
